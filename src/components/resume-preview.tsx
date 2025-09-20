026887a7ccb660e36586c0786c65678b9f2746f7
Yiğit Katırcıoğlu <90yigit@gmail.com>
2025-09-20 15:38:52 +0000

    pdf is empty

--- src/components/resume-preview.tsx
+++ src/components/resume-preview.tsx
@@ -5,16 +5,11 @@ import { ResumePreviewClassic } from "./resume-preview-classic";
 
 export default function ResumePreview() {
   return (
-    <>
-      <div 
-        id="resume-preview-container"
-        className="flex items-start justify-center w-full"
-      >
-        <ResumePreviewClassic />
-      </div>
-       <div id="print-content" className="hidden">
-        <ResumePreviewClassic />
-      </div>
-    </>
+    <div 
+      id="resume-preview-container"
+      className="flex items-start justify-center w-full"
+    >
+      <ResumePreviewClassic />
+    </div>
   );
 }
