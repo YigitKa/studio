c3dead3a12d486e209224e081e0f87b52ce39db6
Yiğit Katırcıoğlu <90yigit@gmail.com>
2025-09-20 13:25:00 +0000

    Try fixing this error: `Build Error: Export ThemeContext doesn't exist i

--- src/contexts/theme-context.tsx
+++ src/contexts/theme-context.tsx
@@ -1,17 +1,12 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, useTheme as useNextTheme } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-
-import { useContext } from "react"
-import {
-  ThemeContext as NextThemeContext,
-} from "next-themes"
 
 type ThemeContextType = {
   theme?: string
@@ -19,7 +14,7 @@ type ThemeContextType = {
 }
 
 export const useTheme = (): ThemeContextType => {
-  const context = useContext(NextThemeContext)
+  const context = useNextTheme()
   if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider")
   }
